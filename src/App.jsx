586038e0494ca3c5b68e22ae0b1fe74983a9102e
ernkeyz <exconzy26@gmail.com>
2023-12-07 15:27:22 +0000

    Play Status

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { Background } from "./Components/Background/Background";
 
 const App = () => {
   const heroData = [
-    { text1: "Dive into", text2: "what you love" },
-    { text1: "Indulge", text2: "your Passion" },
-    { text1: "Give in to", text2: "your passion" },
+    { text1: "Immerse yourself in", text2: "musical creativity" },
+    { text1: "Explore", text2: "your sonic passion" },
+    { text1: "Surrender to", text2: "the art of sound" },
   ];
 
   const [heroCount, setHeroCount] = useState(2);
   const [playStatus, setPlayStatus] = useState(false);
 
-  return <div>App</div>;
+  return;
+  <>
+    <Background playStatus={playStatus} heroCount={heroCount} />
+  </>;
 };
 
 export default App;
--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Background.css";
 
-const Background = () => {
-  return <div>Background</div>;
+import video from "../../assets/video1.mp4";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+
+const Background = ({ playStatus, heroCount }) => {
+  if (playStatus) {
+    return <video className="background" autoPlay loop muted></video>;
+  }
 };
 
 export default Background;
