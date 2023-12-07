f3eaa8618b17f059a00fd77d4ddf623eda11fcaf
ernkeyz <exconzy26@gmail.com>
2023-12-07 15:37:50 +0000

    Hero Background

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -8,7 +8,17 @@ import image3 from "../../assets/image3.png";
 
 const Background = ({ playStatus, heroCount }) => {
   if (playStatus) {
-    return <video className="background" autoPlay loop muted></video>;
+    return (
+      <video className="background" autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
+    );
+  } else if (heroCount === 0) {
+    return <img src={image1} className="background" alt="first_image" />;
+  } else if (heroCount === 1) {
+    return <img src={image2} className="background" alt="second_image" />;
+  } else if (heroCount === 2) {
+    return <img src={image3} className="background" alt="last_image" />;
   }
 };
 
