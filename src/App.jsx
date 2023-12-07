5a7061f52d2e68e86a2bb4f2fcd50e6ae4a654c8
ernkeyz <exconzy26@gmail.com>
2023-12-07 15:13:09 +0000

    Creating useState for Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 const App = () => {
+  const heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your Passion" },
+    { text1: "Give in to", text2: "your passion" },
+  ];
+
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
   return <div>App</div>;
 };
 
