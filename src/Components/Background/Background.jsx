41ffdabbad7ffce0bdf4c8e97e1e3a5af6564d95
ernkeyz <exconzy26@gmail.com>
2023-12-07 17:39:34 +0000

    fade in animation for Images and Video

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -9,16 +9,20 @@ import image3 from "../../assets/image3.png";
 const Background = ({ playStatus, heroCount }) => {
   if (playStatus) {
     return (
-      <video className="background" autoPlay loop muted>
+      <video className="background fade-in" autoPlay loop muted>
         <source src={video} type="video/mp4" />
       </video>
     );
   } else if (heroCount === 0) {
-    return <img src={image1} className="background" alt="first_image" />;
+    return (
+      <img src={image1} className="background fade-in" alt="first_image" />
+    );
   } else if (heroCount === 1) {
-    return <img src={image2} className="background" alt="second_image" />;
+    return (
+      <img src={image2} className="background fade-in" alt="second_image" />
+    );
   } else if (heroCount === 2) {
-    return <img src={image3} className="background" alt="last_image" />;
+    return <img src={image3} className="background fade-in" alt="last_image" />;
   }
 };
 
