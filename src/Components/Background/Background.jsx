968ce7704f7bb049d943d217f53f8c0e59f7adb1
ernkeyz <exconzy26@gmail.com>
2023-12-07 15:02:42 +0000

    Folder && File Structure - Linking CSS files

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Background.css";
 
 const Background = () => {
   return <div>Background</div>;
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
   return <div>Hero</div>;
