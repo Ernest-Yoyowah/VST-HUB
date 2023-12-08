af0003678724a998038bbc698e8cfe2e2a56a204
ernkeyz <exconzy26@gmail.com>
2023-12-08 22:16:06 +0000

    Featured Section

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Featured.css";
 import image from "../../assets/kontakt1.jpg";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Featured = () => {
   // Sample data for featured plugins
   const featuredPlugins = [
@@ -18,17 +20,46 @@ const Featured = () => {
     },
     {
       id: 3,
-      name: "KOMPLETE bundles",
+      name: "KOMPLETE Start",
       imageUrl:
         "https://www.native-instruments.com/typo3temp/pics/img-tile-homepage-featured-product-komplete-start-fa06377ce049d5595e5873ee6d887884-d.jpg",
     },
     {
       id: 4,
-      name: "Instruments & effects",
+      name: "Keyboards",
       imageUrl:
         "https://www.native-instruments.com/typo3temp/pics/img-tile-homepage-featured-products-keyboards-new-b6471e1742981e62ac330935c622a9fd-d.jpg",
     },
-    // Add more plugins as needed
+    {
+      id: 5,
+      name: "MASCHINE MIKRO",
+      imageUrl:
+        "https://www.native-instruments.com/typo3temp/pics/img-tile-homepage-featured-products-maschine-mikro-c32c78b972397c06d08141c32b2b6538-d.jpg",
+    },
+    {
+      id: 6,
+      name: "MASCHINE",
+      imageUrl:
+        "https://www.native-instruments.com/typo3temp/pics/img-tile-homepage-featured-product-maschine-7cb485d1643c2050365288a7f396c29a-d.jpg",
+    },
+    {
+      id: 7,
+      name: "TRAKTOR PRO 3",
+      imageUrl:
+        "https://www.native-instruments.com/typo3temp/pics/img-tile-homepage-featured-products-traktor-pro-3-4a0b01f10c05402823243baea2fa2fd3-d.jpg",
+    },
+    {
+      id: 8,
+      name: "DJ products",
+      imageUrl:
+        "https://www.native-instruments.com/typo3temp/pics/img-tile-homepage-featured-products-dj-products-d6ac3274a7ef577ce74ab25ad991ee4d-d.jpg",
+    },
+    {
+      id: 9,
+      name: "KONTAKT 7",
+      imageUrl:
+        "https://www.native-instruments.com/typo3temp/pics/img-tile-homepage-featured-products-kontakt-7-2-4f3a319d293692e1fa1d6fe06dcf7792-d.jpg",
+    },
   ];
 
   return (
@@ -69,7 +100,7 @@ const Featured = () => {
                         data-media-query="m@2x"
                       />
                       <div className="image-caption text-left">
-                        <h4>{plugin.name}</h4>
+                        <h4 className="plugin-name">{plugin.name}</h4>
                       </div>
                     </div>
                   </a>
