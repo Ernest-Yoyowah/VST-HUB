c02760e5cd2daa03db104a93fd80cabc6b0e3738
ernkeyz <exconzy26@gmail.com>
2023-12-11 20:47:05 +0000

    Homepage Section

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -24,30 +24,127 @@ const Homepage = () => {
             Unlock the full potential of your music with the best piano plugins
             and gear.
           </p>
-          <a href="#featured-plugins" className="cta-button">
+          <a href="#about-keyboard-rig" className="cta-button">
             Explore Now
           </a>
         </div>
       </section>
 
-      {/* Featured Plugins Section */}
-      <section id="featured-plugins" className="featured-plugins">
+      {/* About Keyboard Rig Section */}
+      <section id="about-keyboard-rig" className="about-keyboard-rig section">
         <div className="container">
-          <h2>Featured Piano Plugins</h2>
+          <h2>About Keyboard Rigs</h2>
+          <p>
+            A keyboard rig is the heart of musical expression, combining
+            keyboards, synthesizers, and accessories to create captivating
+            sounds. It's a essential setup for musicians, offering a world of
+            creative possibilities.
+          </p>
+          <p>
+            Build your keyboard rig to unlock your musical potential – whether
+            performing live, recording, or practicing at home. Explore a rich
+            palette of sounds and textures that elevate your music to new
+            heights.
+          </p>
         </div>
       </section>
 
       {/* Build Rig Section */}
-      <section className="build-rig">
+      <section id="featured-plugins" className="featured-plugins">
         <div className="container">
-          <h2>Build Your Piano Rig</h2>
-          <p>What You Need to Build a Virtual Keys Rig</p>
-          {/* Add information about keyboards, MIDI controllers, software, etc. */}
+          <h2>Setting Up a Keyboard Rig for Worship</h2>
+          <p className="section-description">
+            Elevate your worship experience with a carefully crafted keyboard
+            rig. Ensure a harmonious blend of sounds to create a spiritual
+            atmosphere during your services.
+          </p>
+
+          <div className="rig-items">
+            {/* Essential Equipment */}
+            <div className="rig-category">
+              <h3>Essential Equipment</h3>
+
+              <div className="rig-item">
+                <h4>1. Keyboard or Digital Piano</h4>
+                <p>
+                  Choose a high-quality instrument with authentic piano sounds
+                  and features suitable for worship music.
+                </p>
+              </div>
+
+              <div className="rig-item">
+                <h4>2. Synthesizer</h4>
+                <p>
+                  Enhance your sound palette with a synthesizer offering rich
+                  orchestral and ambient tones.
+                </p>
+              </div>
+
+              <div className="rig-item">
+                <h4>3. Worship-Specific Sound Libraries</h4>
+                <p>
+                  Access a variety of choir voices, pads, and atmospheric sounds
+                  designed for worship arrangements.
+                </p>
+              </div>
+
+              <div className="rig-item">
+                <h4>4. Audio Interface</h4>
+                <p>
+                  Connect your rig to a computer or sound system for
+                  high-quality sound output during worship.
+                </p>
+              </div>
+            </div>
+
+            {/* Optional Equipment (but recommended) */}
+            <div className="rig-category">
+              <h3>Optional Equipment (but recommended)</h3>
+
+              <div className="rig-item">
+                <h4>5. Worship Software</h4>
+                <p>
+                  Utilize software tailored for worship music production,
+                  including backing track software and virtual choir
+                  applications.
+                </p>
+              </div>
+
+              <div className="rig-item">
+                <h4>6. Stage Accessories</h4>
+                <p>
+                  Complete your setup with accessories like keyboard stands,
+                  sustain pedals, and comfortable seating.
+                </p>
+              </div>
+
+              <div className="rig-item">
+                <h4>7. Cable Management</h4>
+                <p>
+                  Keep your setup organized with cable management solutions to
+                  avoid distractions during worship.
+                </p>
+              </div>
+
+              <div className="rig-item">
+                <h4>8. Practice and Familiarization</h4>
+                <p>
+                  Dedicate time for practice and become familiar with your
+                  equipment to lead worship seamlessly.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <p className="section-description">
+            With these essential and recommended components, you'll create a
+            powerful and inspiring keyboard rig for worship services.
+          </p>
         </div>
       </section>
 
       {/* Tutorials Section */}
-      <section className="tutorials">
+      <section className="tutorials section">
         <div className="container">
           <h2>Guides and Tutorials</h2>
           <p>
@@ -59,7 +156,7 @@ const Homepage = () => {
       </section>
 
       {/* Community Section */}
-      <section className="community">
+      <section className="community section">
         <div className="container">
           <h2>Join the Community</h2>
           <p>
